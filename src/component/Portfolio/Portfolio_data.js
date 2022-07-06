10cf6ed64a291d9a6d90758c1fc32929bc12c1c9
yousif gbareen <81322967+Yosoftware-engineering@users.noreply.github.com>
2022-07-06 14:48:45 +0300

    Initialize Repository

---
+++ src/component/Portfolio/Portfolio_data.js
@@ -0,0 +1,34 @@
+
+const Portfolio_data = [
+  {
+    id: 1,
+    category: "DEVELOPMENT",
+    totalLike: "23",
+    title: "whatsApp-react native ",
+    image: "/component/pic/whatsApppic.png",
+  },
+  
+  {
+    id: 2,
+    category: "DEVELOPMENT",
+    totalLike: "17",
+    title: "Save Me -android Application",
+    image: "./image/portfolio-02.jpg",
+  },
+  {
+    id: 3,
+    category: "DEVELOPMENT",
+    totalLike: "35",
+    title: "StoreApp- Python ",
+    image: "./image/profile-3.jpg",
+  },
+  {
+    id: 4,
+    category: "DEVELOPMENT",
+    totalLike: "52",
+    title: "School System- java & angular",
+    image: "https://rainbowit.net/html/inbio/assets/images/portfolio/whatsApppic.png",
+  },
+  
+]
+export default Portfolio_data
